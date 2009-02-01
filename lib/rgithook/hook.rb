3d314ba74d90f3d0d74b957680ec38ad61fd30d7
Guillermo Álvarez <guillermo@cientifico.net>
2009-02-01 22:50:49 +0100

    Add method missing to RGitHook::Hook so other hooks like update, pre_commit, etc... can run correctly

--- lib/rgithook/hook.rb
+++ lib/rgithook/hook.rb
@@ -1,36 +1,39 @@
-
-
 module RGitHook
-   # Interface from git-commands to RGitHook
-   class Hook #::nodoc::
+  # Interface from git-commands to RGitHook
+  class Hook #::nodoc::
+    def self.execute
+      puts "Executing #{File.basename($0)} hook"
+      self.send File.basename($0).tr('-','_')
+    end
 
-      def self.execute
-         puts "Executing #{File.basename($0)} hook"
-         self.send File.basename($0).tr('-','_')
+    def self.post_receive
+      packs = STDIN.read.split("\n")
+      ret_vals = []
+
+      rgithook = RGitHook.new repo_path
+
+      packs.each do |pack|
+        oldrev, newrev, ref = pack.split(' ')
+        ret_vals << rgithook.post_receive(oldrev,newrev,ref).first[1].to_i
       end
+      return ret_vals.max
+    end
 
-      def self.post_receive
-         packs = STDIN.read.split("\n")
-
-         ret_vals = []
-
-         rgithook = RGitHook.new repo_path
-
-         packs.each do |pack|
-            oldrev, newrev, ref = pack.split(' ')
-            ret_vals << rgithook.post_receive(oldrev,newrev,ref)
-         end
-         return ret_vals.max
+    def self.method_missing(method_name)
+      RGitHook.new(repo_path).send(method_name)
+      0
+    rescue
+      -1
+    end
+    
+    private
+    def self.repo_path
+      if File.basename(File.expand_path(ENV['GIT_DIR']))  == '.git'
+        repo_path = File.join(ENV['GIT_DIR'],'..')
+      else
+        repo_path = ENV['GIT_DIR']
       end
-
-      private
-      def self.repo_path
-         if File.basename(File.expand_path(ENV['GIT_DIR']))  == '.git'
-            repo_path = File.join(ENV['GIT_DIR'],'..')
-         else
-            repo_path = ENV['GIT_DIR']
-         end
-      end
-   end
+    end
+  end
 end
 
